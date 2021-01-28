da213cc9a2b0bb1745841ba402aa24508c0f635d
Lia Yeh <lia@fullstackquantumcomputation.tech>
2021-01-27 22:48:21 -0800

    credit hardware vector graphics by people in server

--- assets/js/hardware.js
+++ assets/js/hardware.js
@@ -1,13 +1,23 @@
+var left_img_creds = [
+"Left image drawn by Lia Yeh.  Quantum Universal Education.  Open-sourced under ECL-2.0 license",
+"Left image concept by Samanvay Sharma and drawn by Alberto Maldonado Romo.  Quantum Universal Education.  Open-sourced under ECL-2.0 license",
+"Left image drawn by Lia Yeh.  Quantum Universal Education.  Open-sourced under ECL-2.0 license",
+"Left image drawn by Lia Yeh.  Quantum Universal Education.  Open-sourced under ECL-2.0 license",
+"Left image drawn by Alberto Maldonado Romo.  Quantum Universal Education.  Open-sourced under ECL-2.0 license",
+"Left image drawn by Alberto Maldonado Romo.  Quantum Universal Education.  Open-sourced under ECL-2.0 license",
+"Left image drawn by Lia Yeh.  Quantum Universal Education.  Open-sourced under ECL-2.0 license",
+"Left image drawn by Jayanti Singh and Hemant Chaurasia.  Quantum Universal Education.  Open-sourced under ECL-2.0 license",
+"Left image drawn by Alberto Maldonado Romo.  Quantum Universal Education.  Open-sourced under ECL-2.0 license"];
 var left_img_srcs = [
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-""];
+"ions",
+"nmr",
+"diamond_nv",
+"neutral_atoms",
+"photonic",
+"superconducting",
+"topological",
+"cQED",
+"dot"];
 var right_img_srcs = [
 "https://olv.duke.edu/news/ionq-raises-additional-funding-for-its-quantum-computing-platform/",
 "http://www.org.chemie.tu-muenchen.de/glaser/qcomp.html",
@@ -34,7 +44,7 @@ var html_str = "";
 var i;
 for (i = 0; i < left_img_srcs.length; i++) {
   var idx = (first + i) % left_img_srcs.length; 
-  html_str += "<div style=\"border: 0px solid black; width: 65vw; padding: 0px; margin: 0px; text-align: center\"><a href=\"/hardware/" + left_img_srcs[idx].replace(/ /g, "-") + "/\">" + left_img_srcs[idx] + "</a><br><img src=\"/assets/images/hardwares/"+"Slide"+(idx+1)+".PNG"+"\" style=\"width: 65vw\"><a href=\"" + left_img_srcs[idx] + "\"><p style=\"text-align:right; font-size:8px;\">left image source</p></a><a href=\"" + right_img_srcs[idx] + "\"><p style=\"text-align:right; font-size:8px;\">right image source</p></a><a href=\"" + def_srcs[idx] + "\"><p style=\"text-align:right; font-size:8px;\">definition source</p></a></div><br>\n";
+  html_str += "<div style=\"border: 0px solid black; width: 65vw; padding: 0px; margin: 0px; text-align: center\"><a href=\"/hardware/" + left_img_srcs[idx] + "/\"><img src=\"/assets/images/hardwares/"+"Slide"+(idx+1)+".PNG"+"\" style=\"width: 65vw\"></a><a href=\"" + "/assets/images/hardwares/" + left_img_srcs[idx] + ".svg" + "\"><p style=\"text-align:right; font-size:8px;\">" + left_img_creds[idx] + "</p></a><a href=\"" + right_img_srcs[idx] + "\"><p style=\"text-align:right; font-size:8px;\">right image source</p></a><a href=\"" + def_srcs[idx] + "\"><p style=\"text-align:right; font-size:8px;\">definition source</p></a></div><br>\n";
 }
 document.getElementById("hardware").innerHTML = html_str;
 
